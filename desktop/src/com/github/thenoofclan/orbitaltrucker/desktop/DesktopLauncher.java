5cb8300ca58f2d40a2b8586827584601fab1bbd8
jellopudding <zrb1999@hotmail.com>
2016-10-31 20:09:44 -0700

    setup bace display

--- desktop/src/com/github/thenoofclan/orbitaltrucker/desktop/DesktopLauncher.java
+++ desktop/src/com/github/thenoofclan/orbitaltrucker/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.github.thenoofclan.orbitaltrucker.OrbitalTrucker;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Orbital Trucker | <VERSIONNUMBER>";
+		config.foregroundFPS = 60;
+		config.height = 240;
+		config.width = 256;
 		new LwjglApplication(new OrbitalTrucker(), config);
 	}
 }
