36e5e26ee6ac02373185e49de5988ffbbaeadc51
onionsans <artem.astapchuk@gmail.com>
2016-12-20 11:29:10 -0800

    Attempting to get libgdx to load an image

--- core/src/com/github/thenoofclan/orbitaltrucker/OrbitalTrucker.java
+++ core/src/com/github/thenoofclan/orbitaltrucker/OrbitalTrucker.java
@@ -7,31 +7,37 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class OrbitalTrucker extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	public OrthographicCamera camera;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+public class OrbitalTrucker extends ApplicationAdapter
+{
+    public static final int SCALE = 2;
 
-		camera = new OrthographicCamera(240, 256);
-	}
+    SpriteBatch batch;
+    Texture img;
+    public OrthographicCamera camera;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create()
+    {
+        batch = new SpriteBatch();
+        img = new Texture("title.png");
+
+        camera = new OrthographicCamera(240 * SCALE, 256 * SCALE);
+    }
+
+    @Override
+    public void render()
+    {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void dispose()
+    {
+        batch.dispose();
+        img.dispose();
+    }
 }
