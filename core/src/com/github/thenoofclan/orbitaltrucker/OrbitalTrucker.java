ab9a8d1b13646844200750e902d70c0840a7c6a3
onionsans <artem.astapchuk@gmail.com>
2016-12-20 17:28:24 -0800

    Continuing to mess with things

--- core/src/com/github/thenoofclan/orbitaltrucker/OrbitalTrucker.java
+++ core/src/com/github/thenoofclan/orbitaltrucker/OrbitalTrucker.java
@@ -19,15 +19,15 @@ public class OrbitalTrucker extends ApplicationAdapter
     public void create()
     {
         batch = new SpriteBatch();
-        img = new Texture("title.png");
+        img = new Texture(Gdx.files.internal("title.png"));
 
-        camera = new OrthographicCamera(240, 256);
+        camera = new OrthographicCamera(256, 240);
     }
 
     @Override
     public void render()
     {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(img, 0, 0);
--- desktop/src/com/github/thenoofclan/orbitaltrucker/desktop/DesktopLauncher.java
+++ desktop/src/com/github/thenoofclan/orbitaltrucker/desktop/DesktopLauncher.java
@@ -4,13 +4,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.github.thenoofclan.orbitaltrucker.OrbitalTrucker;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Orbital Trucker | <VERSIONNUMBER>";
-		config.foregroundFPS = 60;
-		config.height = 240;
-		config.width = 256;
-		new LwjglApplication(new OrbitalTrucker(), config);
-	}
+public class DesktopLauncher
+{
+    public static void main(String[] arg)
+    {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Orbital Trucker | Sol";
+        config.foregroundFPS = 60;
+        config.height = 240;
+        config.width = 256;
+        new LwjglApplication(new OrbitalTrucker(), config);
+    }
 }
